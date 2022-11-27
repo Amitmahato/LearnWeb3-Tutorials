6e1cfbf4069beebc6218c385fe9006d3a02da0a1
Amit Mahato <amit643.mahato@gmail.com>
2022-11-27 14:20:35 +0545

    Sophomore 10.1 - Build a NFT collection dApp - smart contract

---
+++ Sophomore/10.NFT-Collection/hardhat-tutorial/constants/index.ts
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x9512EEA98279A77e5048F06F6d520919e5573CdB";
+// URL to extract Metadata for a Crypto Dev NFT
+export const METADATA_URL =
+  "https://learn-web3-sophomore-nft-collection.vercel.app/api/";
